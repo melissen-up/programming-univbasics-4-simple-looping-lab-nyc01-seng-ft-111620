7dbf1aa6fe64882658dd1b912aa236d14c4bc4e5
Melissa F <melissa.faulner@gmail.com>
2020-11-01 21:30:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times {puts string}
+end
+
+def loop_message_n_times(string, i)
+  i.times {puts string}
+end
+
+def output_array(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+    array
+end
